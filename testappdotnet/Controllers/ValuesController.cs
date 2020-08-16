641ff4cce6334dc24e2715df1f87598fd0e82ea9
pablo silva <pablo.silva@ey.com>
2020-08-16 19:35:40 -0300

    first commit cors async endpoints

---
+++ testappdotnet/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testappdotnet.Data;
+
+namespace testappdotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _contexts;
+
+        public ValuesController(DataContext context)
+        {
+            _contexts = context;
+        }
+        // GET: api/Values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _contexts.Values.ToList();
+
+            return Ok(values);
+        }
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _contexts.Values.FirstOrDefault(x=> x.Id==id);
+            return Ok(value);
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ testappdotnet/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testappdotnet.Models;
+
+namespace testappdotnet.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options ): base(options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
