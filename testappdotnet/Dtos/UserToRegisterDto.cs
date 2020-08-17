744c8755beb9124f4809e80fdc174c8217a73b94
pablo silva <pablo.silva@ey.com>
2020-08-17 20:34:31 -0300

    added user dtos

--- testappdotnet/Dtos/UserToRegisterDto.cs
+++ testappdotnet/Dtos/UserToRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +10,11 @@ namespace testappdotnet.Dtos
 {
     public class UserToRegisterDto 
     {
+        [Required]
         public string UserName { get; set; }
+        
+        [Required]
+        [StringLength(12,MinimumLength = 8, ErrorMessage ="must provide between 8 and 12 characters")]
         public string Password { get; set; }
     }
 }
