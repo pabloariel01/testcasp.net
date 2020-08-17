932b6ead020e0269e192aead709e623fce8b9345
pablo silva <pablo.silva@ey.com>
2020-08-17 18:56:12 -0300

    added first dto (users)

---
+++ testappdotnet/Dtos/UserToRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testappdotnet.Dtos
+{
+    public class UserToRegisterDto 
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
